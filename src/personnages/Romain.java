68c3d7dedcd821bf5c9d13d67dd7ee44882ec4ba
smoothwastaken <clery.arque-ferradou@univ-tlse3.fr>
2024-02-27 19:21:27 +0100

    Mise-à-jour TP2 de la classe 'Romain' (énumération)

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,6 +4,9 @@ public class Romain {
 	private String nom;
 	private int force;
 	
+	private int nbEquipement = 0;
+	private Equipement[] equipements = new Equipement[2];
+	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
@@ -46,6 +49,32 @@ public class Romain {
 		assert forceAvantCoup > forceApresCoup;
 	}
 	
+	private void ajoutEquipement(Equipement equipement) {
+		this.equipements[this.nbEquipement] = equipement;
+		this.nbEquipement ++;
+		System.out.println("Le soldat " + this.getNom() + " s’équipe avec un " + equipement.toString() + ".");
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (this.nbEquipement) {
+		case 0:
+			this.ajoutEquipement(equipement);
+			break;
+			
+		case 1:
+			if (equipements[0] != null && equipements[0].toString().equals(equipement.toString())) {
+				System.out.println("Le soldat " + this.getNom() + " possède déjà un " + equipement.toString() + " !");
+			} else {
+				this.ajoutEquipement(equipement);
+			}
+			break;
+			
+		case 2:
+			System.out.println("Le soldat " + this.getNom() + " est déjà bien protégé !");
+			break;
+		}
+	}
+	
 	private boolean isInvariantSatisfied() {
 		return this.force >= 0;
 	}
@@ -56,6 +85,14 @@ public class Romain {
 		marcus.parler("Bonjour!");
 		marcus.recevoirCoup(1);
 		marcus.recevoirCoup(2);
+		
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		
+		marcus.sEquiper(Equipement.CASQUE);
+		marcus.sEquiper(Equipement.CASQUE);
+		marcus.sEquiper(Equipement.BOUCLIER);
+		marcus.sEquiper(Equipement.CASQUE);
 		
 	}
 }
