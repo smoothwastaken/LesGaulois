4f5f9295118c7d54702b66e0f0552fc6190edb53
smoothwastaken <clery.arque-ferradou@univ-tlse3.fr>
2024-02-27 18:42:00 +0100

    Mise-à-jour TP2 de la classe 'Romain' (pré, post conditions et les invariants)

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		isInvariantSatisfied();
 	}
 	
 	public String getNom() {
@@ -22,16 +23,35 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		// Pré-condition
+		assert force > 0;
+		
+		// Première valeur variant
+		int forceAvantCoup = force;
+		
 		force -= forceCoup;
+		
+		// Invariant
+		assert isInvariantSatisfied();
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		
+		// Deuxième valeur variant
+		int forceApresCoup = force;
+		
+		// Post-condition (variant)
+		assert forceAvantCoup > forceApresCoup;
+	}
+	
+	private boolean isInvariantSatisfied() {
+		return this.force >= 0;
 	}
 	
 	public static void main(String[] args) {
-		Romain marcus = new Romain("Marcus", 2);
+		Romain marcus = new Romain("Marcus", 6);
 		System.out.println(marcus.prendreParole());
 		marcus.parler("Bonjour!");
 		marcus.recevoirCoup(1);
