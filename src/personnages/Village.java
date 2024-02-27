218dd96e4e3e02dddc3541fff6aa46787f3f051d
smoothwastaken <clery.arque-ferradou@univ-tlse3.fr>
2024-02-27 18:26:38 +0100

    Mise-à-jour TP2 de la classe 'Village'

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -6,9 +6,13 @@ public class Village {
 	private String nom;
 	private Chef chef;
 	
-	public Village(String nom, String nomChef) {
+	private Gaulois[] villageois;
+	private int nbVillageois;
+	
+	public Village(String nom, String nomChef, int forceChef, int nbVillageoisMaximum) {
 		this.nom = nom;
 		this.chef = new Chef(nomChef, new Random().nextInt(10), this);
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
 	public String getNom() {
@@ -19,11 +23,54 @@ public class Village {
 		return chef;
 	}
 	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + this.chef.getNom() + " vivent les légendaires gaulois :");
+		for (int i = 0; i < this.nbVillageois; i ++) {
+			System.out.println("- " + this.villageois[i].getNom());
+		}
+	}
+	
+	public void ajouterHabitant(Gaulois nouveauHabitant) {
+		this.villageois[this.nbVillageois] = nouveauHabitant;
+		this.nbVillageois ++;
+	}
+	
+	public Gaulois trouverHabitant(int indiceHabitant) {
+		return this.villageois[indiceHabitant];
+	}
+	
 	public static void main(String[] args) {
-		Village village = new Village("Sarrant", "Okawa");
-		System.out.println(village.getChef());
-		System.out.println(village.chef.village.chef);
-		System.out.println(village);
-		System.out.println(village.chef.village);
+		Village village = new Village("Village des Irréductibles", "Abraracourcix", 6, 30);
+		
+		/* Quand on ajoute la ligne suivante:
+		 * Gaulois gaulois = village.trouverHabitant(30);
+		 * On obtient l'erreur suivante:
+		 * Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 30 out of bounds for length 30
+		 *	at personnages.Village.trouverHabitant(Village.java:32)
+		 *	at personnages.Village.main(Village.java:37)
+		 *
+		 * Cette erreur survient car on essaye d'accéder au 30e élément
+		 * alors qu'il n'y en a que 29.
+		 */
+		
+		// Création du chef lors de l'instanciation du village (contrainte supplémentaire imposée lors du TP1)
+		
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		
+		/*
+		 * Gaulois gaulois = village.trouverHabitant(1);
+		 * System.out.println(gaulois);
+		 * 
+		 * 'gaulois' est null, car on accède au deuxième habitant du village
+		 * alors que le village n'en dispose que d'un.
+		 */
+		
+		village.afficherVillageois();
+		
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+
 	}
 }
